285ecb1109e22352c86ac8d2e6f8237c77bdbf01
Kien <hoangkientrung2@gmail.com>
2025-02-20 20:50:56 +0700

    update

--- public/contact.js
+++
@@ -1,5 +0,0 @@
-function sendEmail(email) {
-    const subject = encodeURIComponent("Hello from FakeWeather!");
-    const body = encodeURIComponent("Hi, I would like to get in touch with you.");
-    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
-}
